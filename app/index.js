e0be5697d00b8b93fda90ebb99747b1feeb0310d
aleksandriprifti <prifti.aleksandri@gmail.com>
2023-11-14 16:55:16 +0100

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+import { Link, useRouter } from "expo-router";
+
+const LoginPage = () => {
+  const router = useRouter();
+
+  const handlePress = () => {
+    router.replace("app");
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Pressable onPress={handlePress}>
+        <Text>Login</Text>
+      </Pressable>
+      <Link href={"/register"} asChild>
+        <Pressable>
+          <Text>Create Account</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
+};
+
+export default LoginPage;
---
+++ app/register.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const register = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>RegisterPage</Text>
+    </View>
+  );
+};
+
+export default register;
